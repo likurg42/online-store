78958912bee5ea5f288c106540c0a2ae538ee89a
AndreiPavlov135 <9000andrey@gmail.com>
2022-12-26 23:59:31 +0300

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Header from './components/header/Header';
 
 export default function App() {
-    return <h1>Hi</h1>;
+    return <Header />;
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import cl from './header.module.scss';
+import logo from './header-images/logo.png';
+import bag from './header-images/icon-bag.png';
+import like from './header-images/icon-like.png';
+import search from './header-images/icon-search.png';
+
+export default function Header() {
+    return (
+        <div className={cl.header}>
+            <img className={cl.logo} src={logo} alt="logo" />
+            <div className={cl.nav}>
+                <div>HOME</div>
+                <div>SHOP</div>
+                <div>BLOG</div>
+                <div>SALE</div>
+                <div>CONTACT US</div>
+                <img className={cl.search} src={search} alt="search" />
+                <div>SEARCH</div>
+            </div>
+            <div className={cl.account}>
+                <div>SING IN</div>
+                <div>CREATE IN ACCOUNT</div>
+                <img src={like} alt="like" />
+                <img src={bag} alt="bag" />
+                <div className="current-buy">
+                    <div>Shoping Cart</div>
+                    <div>
+                        <span>0.00</span>
+                        <span> EUR</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
