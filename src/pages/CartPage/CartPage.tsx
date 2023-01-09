deb33f0fb1692ddb98326b6776b528391d836812
AndreiPavlov135 <9000andrey@gmail.com>
2023-01-09 22:03:40 +0300

    feat: add cart page template

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import cl from './CartPage.module.scss';
 
 export default function CartPage() {
-    return <h1>Shopping Cart</h1>;
+    return (
+        <section>
+            <main className={cl.main}>
+                <h1 className={cl.h1}>Shopping Cart</h1>
+                <div className={cl.captions}>
+                    <span>Product</span>
+                    <span>Price</span>
+                    <span>Size</span>
+                    <span>Quantity</span>
+                    <span>Total</span>
+                </div>
+                <div className={cl.product}>
+                    <div className={cl.product__img}>test</div>
+                    <div className={cl.product__title}>test</div>
+                    <div className={cl.product__price}>test</div>
+                    <div className={cl.product__size}>test</div>
+                    <div className={cl.product__counter}>cointer</div>
+                    <div className={cl.product__total}>test</div>
+                    <div className={cl.product__remove}>x</div>
+                </div>
+                <div className={cl.buttons}>
+                    <button className={cl.btn} type="button">
+                        continue shopping
+                    </button>
+                    <button className={cl.btn} type="button">
+                        clear shopping cart
+                    </button>
+                </div>
+            </main>
+        </section>
+    );
 }
